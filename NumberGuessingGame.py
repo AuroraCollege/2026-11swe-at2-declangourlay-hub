128affc4e1efe93a2a05d872a78e450f657b06d8
Your Name <declan.gourlay997@education.nsw.gov.au>
2026-06-06 04:56:12 +0000

    Created NumberGuessingGame class and connected it to Game parent class

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -4,3 +4,10 @@ class Game:
 
     def play_turn(self, form):
         raise NotImplementedError("Subclasses must implement play_turn().")
+
+class NumberGuessingGame(Game):
+    def __init__(self):
+        super().__init__()
+        self.__target = None
+        self.attempts = 0
+        self.new_game()
